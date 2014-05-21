d1dac3166015def11aa4a72e824ee2d4058009d8
Garrett Oreilly <garrettoreilly@icloud.com>
2014-05-21 04:27:20 -0400

    Trying another thing

--- bot.js
+++ bot.js
@@ -1,3 +1,18 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 var config = {
     channels: ["#chat"],
     server: "mccs.stu.marist.edu",
