854bce52ea98a442cb5e185a417f0015fb024694
Garrett Oreilly <garrettoreilly@icloud.com>
2014-05-26 19:38:06 -0400

    Unindented non-declaration

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ var nr = require('newrelic'),
     plus_lb = new Leaderboard('pluses', {}, client),
     redisURL = url.parse(process.env.REDISCLOUD_URL),
     client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-    client.auth(redisURL.auth.split(":")[1]);
+client.auth(redisURL.auth.split(":")[1]);
 
 var users = [];
 
