56372591fa3aa19473f105a69bdb450048113b2c
Garrett Oreilly <garrettoreilly@icloud.com>
2014-05-28 20:16:52 -0400

    Fixed PM listener

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ bot.addListener("nick", function(oldnick, newnick, channels, message) {
 });
 
 bot.addListener("message", function(nick, to, text, message) {
-    if (/credits/.test(text)) {
+    if (to === bot.botName && /credits/.test(text)) {
         plus_lb.score(nick, function(err, score) {
             bot.say(nick, score.toString());
         });
